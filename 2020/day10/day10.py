2327167192ec91eb41997b15682ac774f94e507d
Dariusz Smigiel <1412617+dasm@users.noreply.github.com>
2020-12-10 14:10:48 -0800

    AoC 2020, Day10, part2 (thanks to arkoz)

--- 2020/day10/day10.py
+++ 2020/day10/day10.py
@@ -1,38 +1,34 @@
 #!/usr/bin/env python3
+from collections import defaultdict
 import sys
+
 
 def load_data(filename):
     with open(filename) as file_:
         output = file_.readlines()
 
     output = map(int, output)
-    return sorted(output)
+    output = sorted(output)
+    output.insert(0, 0)
+    output.append(output[-1]+3)
+    return output
 
-def count_jolts(data):
-    diff_1 = 0
-    diff_2 = 0
-    diff_3 = 0
 
-    value = 0
-    index = 0
-    while index < len(data):
-        next_value = data[index]
-        tmp_value = next_value - value
-        if tmp_value not in [1, 2, 3]:
-            print("Oh no")
-            return None
+def part1(data):
+    joltages = defaultdict(int)
+    for prev, curr in zip(data, data[1:]):
+        joltages[curr-prev] += 1
+    return joltages[1] * joltages[3]
 
-        if tmp_value == 1: diff_1 += 1
-        if tmp_value == 2: diff_2 += 1
-        if tmp_value == 3: diff_3 += 1
+def part2(data):
+    total = {value: 0 for value in data}
+    total[0] = 1
 
-        value = data[index]
-        index += 1
-
-    # extra 3 jolts at the end
-    diff_3 += 1
-    print(diff_1, diff_2, diff_3)
-    return diff_1 * diff_3
+    for a in data:
+        for i in range(1, 4):
+            if a-i in total:
+                total[a] += total[a-i]
+    return total[data[-1]]
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -40,5 +36,7 @@ if __name__ == '__main__':
 
     filename = sys.argv[1]
     output = load_data(filename)
-    part1 = count_jolts(output)
+    part1 = part1(output)
     print('part1', part1)
+    part2 = part2(output)
+    print('part2', part2)
