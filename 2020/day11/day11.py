8d74762e7cd2714d6c88b0269158e02138d89bf4
Dariusz Smigiel <1412617+dasm@users.noreply.github.com>
2020-12-11 06:36:22 -0800

    AoC 2020, Day11, part2

--- 2020/day11/day11.py
+++ 2020/day11/day11.py
@@ -5,11 +5,8 @@ class Cell:
     def __repr__(self): return self.state
     def is_dead(self): return self.state == 'L'
     def is_alive(self): return self.state == '#'
-
     def set_alive(self): self.state = '#'
     def set_dead(self): self.state = "L"
-
-    def print_char(self): return self.state
 
 class Board:
     def __init__(self, data):
@@ -24,10 +21,11 @@ class Board:
             grid.append(column)
         self.grid = grid
 
-    def draw_board(self):
+    def draw(self):
         for row in self.grid:
             for column in row:
-                print(column.print_char(), end='')
+                print(column.state, end='')
+        print()
 
     def check_neighbours(self, check_row, check_col):
         search_min = -1
@@ -49,7 +47,7 @@ class Board:
                     neighbour_list.append(self.grid[neighbour_row][neighbour_col])
         return neighbour_list
 
-    def update_board(self):
+    def update(self, alive):
         goes_dead = []  # empty
         goes_alive = []  # occupied
 
@@ -63,7 +61,7 @@ class Board:
                         living_cells.append(neighbour_cell)
 
                 cell = self.grid[row][col]
-                if cell.is_alive() and len(living_cells) >= 4:
+                if cell.is_alive() and len(living_cells) >= alive:
                     goes_dead.append(cell)
                 if cell.is_dead() and len(living_cells) == 0:
                     goes_alive.append(cell)
@@ -84,6 +82,44 @@ class Board:
                     alive += 1
         return alive
 
+class Board1(Board):
+    pass
+
+ORDER = {
+    "N": lambda x, y: (x, y-1),
+    "S": lambda x, y: (x, y+1),
+    "E": lambda x, y: (x+1, y),
+    "W": lambda x, y: (x-1, y),
+    "NE": lambda x, y: (x+1, y-1),
+    "SE": lambda x, y: (x+1, y+1),
+    "SW": lambda x, y: (x-1, y+1),
+    "NW": lambda x, y: (x-1, y-1),
+}
+class Board2(Board):
+    def _iterate_ray(self, check_row, check_col, order):
+        row = check_row
+        col = check_col
+        while (0 <= row < self.rows) and (0 <= col < self.cols):
+            if not ((row == check_row) and (col == check_col)):
+                cell = self.grid[row][col]
+                if cell.is_dead() or cell.is_alive():
+                    return {order: cell}
+            func = ORDER[order]
+            row, col = func(row, col)
+        return None
+
+    def check_neighbours(self, check_row, check_col):
+        neighbour_list = []
+        neighbour_list = {}
+
+        for order in ('N', 'S', 'W', 'E', 'NE', 'SE', 'SW', 'NW'):
+            neighbours = self._iterate_ray(check_row, check_col, order)
+            if neighbours:
+                neighbour_list.update(neighbours)
+
+        return neighbour_list.values()
+
+
 def load_data(filename):
     with open(filename) as file_:
         data = file_.readlines()
@@ -92,10 +128,12 @@ def load_data(filename):
 if __name__ == '__main__':
     filename = sys.argv[1]
     data = load_data(filename)
-    board = Board(data)
+    board = Board1(data)
     while board.is_changed:
-        #board.draw_board()
-        board.update_board()
-        #print()
+        board.update(alive=4)
+    print('part1', board.count_alive())
 
-    print(board.count_alive())
+    board = Board2(data)
+    while board.is_changed:
+        board.update(alive=5)
+    print('part2', board.count_alive())
