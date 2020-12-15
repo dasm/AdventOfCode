1c72a769ce7feb78281f776079626c364a86ca44
Dariusz Smigiel <1412617+dasm@users.noreply.github.com>
2020-12-15 12:17:57 -0800

    AoC 2020, Day15, part2

--- 2020/day15/day15.py
+++ 2020/day15/day15.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from collections import defaultdict
 
-def part1(data):
+def part1(data, year):
     mem = defaultdict(list)
     for idx, d in enumerate(data):
         mem[d].append(idx)
@@ -11,8 +11,8 @@ def part1(data):
     mem[value].append(idx)
     idx += 1
 
-    while idx < 2021:
-        if idx == 2020:
+    while idx < year+1:
+        if idx == year:
             return value
         values = mem.get(value, [0, 0])
         try:
@@ -24,19 +24,35 @@ def part1(data):
 
 
 if __name__ == '__main__':
-    sample = [0,3,6]
-    print('part1', part1(sample))
-    sample = [1,3,2]
-    print('part1', part1(sample))
-    sample = [2,1,3]
-    print('part1', part1(sample))
-    sample = [1,2,3]
-    print('part1', part1(sample))
-    sample = [2,3,1]
-    print('part1', part1(sample))
-    sample = [3,2,1]
-    print('part1', part1(sample))
-    sample = [3,1,2]
-    print('part1', part1(sample))
+    #sample = [0,3,6]
+    #print('part1', part1(sample, 2020))
+    #sample = [1,3,2]
+    #print('part1', part1(sample, 2020))
+    #sample = [2,1,3]
+    #print('part1', part1(sample, 2020))
+    #sample = [1,2,3]
+    #print('part1', part1(sample, 2020))
+    #sample = [2,3,1]
+    #print('part1', part1(sample, 2020))
+    #sample = [3,2,1]
+    #print('part1', part1(sample, 2020))
+    #sample = [3,1,2]
+    #print('part1', part1(sample, 2020))
     input_ = [0,14,6,20,1,4]
-    print('part1', part1(inputinput_))
+    print('part1', part1(input_, 2020))
+    #sample = [0,3,6]
+    #print('part2', part1(sample, 30000000))
+    #sample = [1,3,2]
+    #print('part2', part1(sample, 30000000))
+    #sample = [2,1,3]
+    #print('part2', part1(sample, 30000000))
+    #sample = [1,2,3]
+    #print('part2', part1(sample, 30000000))
+    #sample = [2,3,1]
+    #print('part2', part1(sample, 30000000))
+    #sample = [3,2,1]
+    #print('part2', part1(sample, 30000000))
+    #sample = [3,1,2]
+    #print('part2', part1(sample, 30000000))
+    input_ = [0,14,6,20,1,4]
+    print('part2', part1(input_, 30000000))
